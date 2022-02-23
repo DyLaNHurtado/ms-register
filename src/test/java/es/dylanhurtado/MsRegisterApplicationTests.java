b8e3816a4d354c458591145f43e853054e5fda4b
Dylan Hurtado López <dylahurtado43@gmail.com>
2022-02-23 21:57:15 +0100

    first commit

---
+++ src/test/java/es/dylanhurtado/MsRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package es.dylanhurtado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
